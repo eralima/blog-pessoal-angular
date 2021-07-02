7940c305d62c71917767c7ac5ea3bf2104d19737
eralima <erikalmlima@gmail.com>
2021-07-02 18:10:18 -0300

    Implementação dos métodos GET e POST de tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,16 +10,37 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
+  tema: Tema =  new Tema()
+  listaTemas: Tema[]
 
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       //alert('Sua sessão expirou, faça o login novamente!')
+      
       this.router.navigate(['/entrar'])
+
     }
+    this.todosTemas()
+    this.temaService.refreshToken()
+  }
+
+  todosTemas(){
+      this.temaService.todosTemas().subscribe((resposta: Tema []) => {
+      this.listaTemas = resposta
+    })
+  }
+  cadastrarTema(){
+    this.temaService.inserirTema(this.tema).subscribe((resposta: Tema) => {
+      this.tema = resposta
+      alert('Tema cadastrado com sucesso!')
+      this.todosTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
