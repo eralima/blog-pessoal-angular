e5e7de5e5ee0dac936fa60bb0d4811b78193979a
eralima <erikalmlima@gmail.com>
2021-07-02 12:22:44 -0300

    Implementação do componente inicio

--- src/app/service/autenticacao.service.ts
+++ src/app/service/autenticacao.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -20,5 +21,13 @@ export class AutenticacaoService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastro', usuario)
+  }
+
+  logado(){
+    let logado: boolean = false
+    if(environment.token != ''){
+      logado =true
+    }
+    return logado
   }
 }
